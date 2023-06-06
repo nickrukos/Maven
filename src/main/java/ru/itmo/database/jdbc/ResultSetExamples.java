37f2c2faea5415c69d2355b5f0ce1326f6bfe8a5
Dasha <dasha>
2023-06-06 17:50:44 +0300

    CachedRowSet

--- src/main/java/ru/itmo/database/jdbc/ResultSetExamples.java
+++ src/main/java/ru/itmo/database/jdbc/ResultSetExamples.java
@@ -1,4 +1,21 @@
 package ru.itmo.database.jdbc;
 
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
+import java.sql.SQLException;
+
+import static ru.itmo.database.jdbc.Settings.*;
+
 public class ResultSetExamples {
+    // сохраняет работоспособность и тогда, когда соединение с базой уже закрыто,
+    // сразу кэшируя в память все данные, которые вернул запрос
+    private static CachedRowSet cachedRowSet() throws SQLException {
+        CachedRowSet cachedRowSet = RowSetProvider.newFactory().createCachedRowSet();
+        cachedRowSet.setUrl(CONNECTION_STR);
+        cachedRowSet.setUsername(LOGIN);
+        cachedRowSet.setPassword(PWD);
+        cachedRowSet.setCommand("SELECT * FROM course");
+        cachedRowSet.execute();
+        return cachedRowSet;
+    }
 }
