b3673270c25f6ad6338c96e8d6976cbe43c5d8c9
Nicholas Rukosuev <nickrukos@mail.ru>
2023-06-16 00:01:08 +0300

    Курсовая 4

---
+++ src/main/java/ru/itmo/database/curs4/C3P0DataSource.java
@@ -0,0 +1,34 @@
+package ru.itmo.database.curs4;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class C3P0DataSource {
+    private C3P0DataSource(){}
+
+    // пул соединений
+    private static ComboPooledDataSource connectionsPool = new ComboPooledDataSource();
+
+    /*
+    static { // настройки, переопределяющие или дополняющие настройки в properties файле
+        try {
+            connectionsPool.setDriverClass("org.postgresql.Driver");
+            connectionsPool.setJdbcUrl("jdbc:postgresql://localhost:5432/lessons");
+            connectionsPool.setUser("jjd");
+            connectionsPool.setPassword("itmo");
+            connectionsPool.setInitialPoolSize(5); // изначальное количество соединений. default = 3
+            connectionsPool.setMinPoolSize(4); // минимальное количество соединений в пуле. default = 3
+            connectionsPool.setMaxPoolSize(10); // максимальное количество соединений в пуле. default = 15
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+    }
+    */
+    public static Connection getConnection() throws SQLException {
+        return connectionsPool.getConnection(); // получение объекта соединения из пула
+        // аналогично: Connection connection = DriverManager.getConnection(CONNECTION_STR, LOGIN, PWD)
+    }
+
+}
---
+++ src/main/java/ru/itmo/database/curs4/DBRequestWithPool.java
@@ -0,0 +1,26 @@
+package ru.itmo.database.curs4;
+
+import ru.itmo.database.connsectionspool.C3P0DataSource;
+import ru.itmo.database.entity.Course;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBRequestWithPool {
+    public static int insertWithPool(Course course){
+        String insertSQL = "INSERT INTO tb_courses (title, duration, price) VALUES(?, ?, ?) RETURNING id";
+        try (PreparedStatement statement = C3P0DataSource.getConnection().prepareStatement(insertSQL)) {
+            statement.setString(1, course.getTitle());
+            statement.setDouble(2, course.getDuration());
+            statement.setInt(3, course.getPrice());
+            try (ResultSet keys = statement.executeQuery()) {
+                if (!keys.next()) return 0;
+                return keys.getInt("id");
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not execute the request: " + e.getSQLState() + ", " + e.getMessage());
+            return 0;
+        }
+    }
+}
